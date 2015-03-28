4221d2ff1072041230d977c86364027349e51b42
Zsolt Lattmann <zsolt.lattmann@gmail.com>
2015-03-28 17:45:08 -0500

    Google auth is not mandatory anymore.

--- server/auth.js
+++ server/auth.js
@@ -93,63 +93,78 @@ function init(app, logger, config) {
         },
 
         google,
-        originalAuthorizationParams;
-    logger.debug('Google OAuth2 is initializing');
+        originalAuthorizationParams,
+
+        useGoogle = false;
+
+    logger.info('Google OAuth2 is initializing');
     if (config.authentication.GOOGLE_CLIENT_ID && config.authentication.GOOGLE_CLIENT_SECRET) {
         googleStrategyConfig.clientID = config.authentication.GOOGLE_CLIENT_ID;
         googleStrategyConfig.clientSecret = config.authentication.GOOGLE_CLIENT_SECRET;
+
+        useGoogle = true;
     } else {
-        throw new Error('GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET have to be defined config.authentication.GOOGLE_*');
+        if (config.authentication.allowGuests) {
+            logger.error('Google OAuth2 is failed to initialize. ' +
+            'GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET have to be defined config.authentication.GOOGLE_*');
+            useGoogle = false;
+        } else {
+            throw new Error('Enable guests config.authentication.allowGuests=true or ' +
+            'GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET have to be defined config.authentication.GOOGLE_*');
+        }
     }
 
+    if (useGoogle) {
+        google = new GoogleStrategy(googleStrategyConfig,
+            function (accessToken, refreshToken, params, profile, done) {
+                var userOriginal = {
+                    id: profile.id,
+                    displayName: profile.displayName,
+                    name: profile.name,
+                    emails: profile.emails
+                };
 
-    google = new GoogleStrategy(googleStrategyConfig,
-        function (accessToken, refreshToken, params, profile, done) {
-            var userOriginal = {
-                id: profile.id,
-                displayName: profile.displayName,
-                name: profile.name,
-                emails: profile.emails
-            };
+                users.update({id: profile.id}, userOriginal, {upsert: true}, function (err, numReplaced) {
+                    users.findOne({id: profile.id}, function (err, user) {
+                        if (err) {
+                            return done(err);
+                        }
 
-            users.update({id: profile.id}, userOriginal, {upsert: true}, function (err, numReplaced) {
-                users.findOne({id: profile.id}, function (err, user) {
-                    if (err) {
-                        return done(err);
-                    }
-
-                    // FIXME: should we check if user is null here?
-                    done(null, userOriginal);
+                        // FIXME: should we check if user is null here?
+                        done(null, userOriginal);
+                    });
                 });
-            });
-        }
-    );
+            }
+        );
 
-    // Monkey patch to support openid.real option
-    originalAuthorizationParams = google.authorizationParams;
+        // Monkey patch to support openid.real option
+        originalAuthorizationParams = google.authorizationParams;
 
-    google.authorizationParams = function () {
-        var val = originalAuthorizationParams.apply(this, arguments);
-        val['openid.realm'] = config.server.publicUrl;
-        return val;
-    };
+        google.authorizationParams = function () {
+            var val = originalAuthorizationParams.apply(this, arguments);
+            val['openid.realm'] = config.server.publicUrl;
+            return val;
+        };
 
-    app.get('/auth/google', passport.authenticate('google', {
-        scope: [
-            'https://www.googleapis.com/auth/userinfo.profile',
-            'https://www.googleapis.com/auth/userinfo.email'
-        ]
-    }));
+        app.get('/auth/google', passport.authenticate('google', {
+            scope: [
+                'https://www.googleapis.com/auth/userinfo.profile',
+                'https://www.googleapis.com/auth/userinfo.email'
+            ]
+        }));
 
-    app.get('/auth/google/oauth2callback',
-        passport.authenticate('google', {successRedirect: '/auth/success', failureRedirect: '/auth/failed'}));
+        app.get('/auth/google/oauth2callback',
+            passport.authenticate('google', {successRedirect: '/auth/success', failureRedirect: '/auth/failed'}));
 
-    logger.debug('Google OAuth2 is ready');
+        logger.info('Google OAuth2 is ready');
+    }
     // GOOGLE SPECIFIC ends here
 
 
     logger.debug('Adding auth strategies');
-    passport.use(google);
+    if (useGoogle) {
+        passport.use(google);
+    }
 
     passport.serializeUser(function (user, done) {
         done(null, user.id);
